240c01e88a0124b0de41d065ad3b12b788e2cf31
grcjp <YOUR_GITHUB_EMAIL>
2026-06-09 12:24:36 -0400

    lab 2.4: compliant GCS bucket module with CMEK, two consumers, evidence

---
+++ terraform/primitives/compliant-gcs/main.tf
@@ -0,0 +1,25 @@
+# primitives/compliant-gcs/main.tf
+terraform {
+  required_version = ">= 1.6"
+  required_providers {
+    google = { source = "hashicorp/google", version = "~> 5.0" }
+  }
+}
+
+provider "google" {
+  project = "cgep-capstone"
+  region  = "us-central1"
+}
+
+module "data_bucket" {
+  source = "../../modules/compliant-gcs-bucket"
+
+  gcp_project        = "cgep-capstone"
+  project_label      = "cgep-lab"
+  environment        = "dev"
+  retention_days     = 30
+  bucket_name_suffix = "dev-data-jleepe"
+}
+
+output "attestation" { value = module.data_bucket.compliance_attestation }
+output "bucket_url"  { value = module.data_bucket.bucket_url }
